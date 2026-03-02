09248ea4199791e651c26d188f77324f5dfd9c7c
netizens-aadyanshi <aadyanshi.p@netizens.email>
2026-03-02 18:11:45 +0530

    withdefault feature1

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,7 +21,7 @@ class Product extends Model
 
     public function category(){
 
-        return $this->belongsTo(Category::class)->withDefault('Uncategorized');
+        return $this->belongsTo(Category::class);
     }
 
     public function productImages(){
